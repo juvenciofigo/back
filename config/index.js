53193212a936f09372298e150feeb7089e9c6467
juvenciofigo <juvenciofigo@gmail.com>
2024-07-14 19:12:04 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: process.env.SECRET,
     api: process.env.NODE_ENV === "production" ? process.env.URI : "http://localhost:3000",
-    loja: process.env.NODE_ENV === "production" ? "https://loja-teste.ampliee.com" : "http://localhost:8000",
+    loja: process.env.NODE_ENV === "production" ? "https://cuddly-tigers-help.loca.lt" : "http://localhost:8000",
 };
