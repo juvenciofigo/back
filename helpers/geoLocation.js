8e6765d4ba40336f51fce29250081f9a3b7ee966
juvenciofigo <juvenciofigo@gmail.com>
2025-04-15 13:20:09 +0200

    Update geoLocation.js

--- helpers/geoLocation.js
+++ helpers/geoLocation.js
@@ -2,17 +2,15 @@ const redis = require("../controllers/integracoes/redisClient");
 const axios = require("axios");
 
 async function getLocationFromIP(ip) {
+    if (ip === "::1") return null;
     const cached = await redis.get(`ip:${ip}`);
 
     if (cached) {
-        console.log("cached",cached);
         return JSON.parse(cached);
     }
 
     const response = await axios.get(`https://ipapi.co/${ip}/json/`);
     const { country_name, region, city } = response.data;
-
-    console.log("response",response);
 
     const location = {
         country: country_name || "Moçambique",
@@ -20,7 +18,7 @@ async function getLocationFromIP(ip) {
         city: city,
     };
 
-    // Cache por 24 horas (86400 segundos)
+    // Cache por 24*2 horas (86400*2 segundos)
     await redis.set(`ip:${ip}`, JSON.stringify(location), "EX", 86400 * 2);
 
     return location;
