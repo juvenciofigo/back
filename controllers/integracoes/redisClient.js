98aea6669653638edc18ee49efd6ada78eb0cb53
juvenciofigo <juvenciofigo@gmail.com>
2025-07-05 04:18:11 +0300

    Update redisClient.js

--- controllers/integracoes/redisClient.js
+++ controllers/integracoes/redisClient.js
@@ -1,31 +1,38 @@
 const Redis = require("ioredis");
 
-const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
-    maxRetriesPerRequest: null,
-    lazyConnect: false,
-    enableOfflineQueue: false,
-    tls: {}, // necessário para conexões TLS (Upstash usa TLS)
-});
-
-redis.on("error", (err) => {
-    console.error("[Redis Error]:", err.message);
-});
-
 async function setCache(key, value, ttl = 86400 * 2) {
+    const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+        maxRetriesPerRequest: 1,
+        lazyConnect: false,
+        enableOfflineQueue: false,
+        tls: {}, // necessário para Upstash
+    });
+
     try {
         await redis.set(key, JSON.stringify(value), "EX", ttl);
     } catch (err) {
         console.error("Erro ao definir cache:", err.message);
+    } finally {
+        await redis.quit(); // encerra corretamente a conexão
     }
 }
 
 async function getCache(key) {
+    const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+        maxRetriesPerRequest: 1,
+        lazyConnect: false,
+        enableOfflineQueue: false,
+        tls: {}, // necessário para Upstash
+    });
+
     try {
         const data = await redis.get(key);
         return data ? JSON.parse(data) : null;
     } catch (err) {
         console.error("Erro ao buscar cache:", err.message);
         return null;
+    } finally {
+        await redis.quit(); // encerra corretamente a conexão
     }
 }
 
